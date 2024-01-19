65cd43b3fc47f8a0089dba8e4594506638dccbd3
namgy1 <namgiyun535@gmail.com>
2024-01-20 04:09:44 +0900

    0120(WeatherUI, DateUI)
    Add WeatherUI, DateUI prototype

---
+++ Assets/Scripts/DateUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DateUI : MonoBehaviour
+{
+    public TextMeshProUGUI dayText;
+    private int dayCount = 0;
+    public WeatherUI weatherUI;
+
+    void Start()
+    {
+        UpdateDayText();
+    }
+
+    void Update()
+    {
+        // 예를 들어, 사용자가 어떤 이벤트를 트리거할 때마다 일 수를 증가시킬 수 있습니다.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IncreaseDayCount();
+        }
+    }
+
+    void IncreaseDayCount()
+    {
+        // 일 수를 증가시키는 메서드를 만들 '예정'
+        UpdateDayText();
+    }
+
+    void UpdateDayText()
+    {
+        dayCount = weatherUI.GetDateCount() + 1;
+        // UI에 현재 일 수를 표시하는 메서드
+        dayText.text = dayCount.ToString() + "일차";
+    }
+}
---
+++ Assets/Scripts/WeatherUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI; // UI 네임스페이스 추가
+using System.IO;
+using TMPro;
+
+public class WeatherUI : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshProUGUI; // TextMeshProUGUI를 사용하는 경우
+    int date = 0; // 이벤트를 하나씩 넘길 때마다 date++
+
+    void Start()
+    {       
+        string filePath = "Assets/Scripts/weather.txt";
+        string[] textLines = System.IO.File.ReadAllLines(filePath);
+
+        string textValue = textLines[date];
+
+        if (textMeshProUGUI != null)
+        {
+            textMeshProUGUI.text += textValue;
+        }
+        else
+        {
+            Debug.LogError("TextMeshProUGUI가 할당되지 않았습니다.");
+        }
+    }
+    public int GetDateCount()
+    {
+        return date;
+    }
+}
